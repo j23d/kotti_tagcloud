d804ddfbc25c8403d258061e8aba20d3c4c8d135
Marco Scheidhuber <j23d@jusid.de>
2014-02-25 21:33:14 +0100

    Initialize the canvas when the document was loaded.

--- kotti_tagcloud/static/js/script.js
+++ kotti_tagcloud/static/js/script.js
@@ -1,34 +1,26 @@
-/* Put your content type specific Javascript here. */
-
+// For more informations and a list of the possible options have
+// a look to http://www.goat1000.com/tagcanvas.php.
 $(function() {
-    function start_canvas() {
-
-        // var content_width = jQuery('.container-fluid').width() * 0.5;
-        // var stretch_x = content_width / 300;
-        // jQuery('#tags-canvas').attr('width', content_width);
-        // jQuery('#canvas-container').attr('width', content_width + 'px');
-        // http://www.goat1000.com/tagcanvas.php
-        if(!jQuery('#tags-canvas').tagcanvas({
-            textColour : '#000',
-            outlineColour: '#8BA6CF',
-            outlineThickness: 3,
-            outlineOffset: 3,
-            maxSpeed : 0.05,
-            freezeActive : false,
-            //stretchX: stretch_x,
-            decel: 0.99, // 0.95
-            // tooltip: 'div',
-            // tooltipClass: 'tags-tooltip',
-            // tooltipDelay: 100,
-            initial: [0.8, -0.3],
-            noMouse: false,
-            wheelZoom: false,
-            imageScale: null,
-            depth : 0.9
-        }, 'tags')) {
-            // TagCanvas failed to load
-            $('#canvas-container').hide();
+    $(document).ready(function() {
+        function start_canvas() {
+            if(!jQuery('#tags-canvas').tagcanvas({
+                textColour : '#000000',
+                outlineColour: '#cccccc',
+                outlineThickness: 3,
+                outlineOffset: 3,
+                maxSpeed : 0.05,
+                freezeActive : false,
+                decel: 0.99,
+                initial: [0.8, -0.3],
+                noMouse: false,
+                wheelZoom: false,
+                imageScale: null,
+                depth : 0.9
+            }, 'tags')) {
+                // TagCanvas failed to load
+                $('#canvas-container').hide();
+            }
         }
-    }
-    start_canvas();
+        start_canvas();
+    });
 });
